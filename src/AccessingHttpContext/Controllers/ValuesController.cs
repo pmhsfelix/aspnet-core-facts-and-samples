5cc3ef440e9a4364992465252a1e9438cec7a394
Pedro Felix <pmhsfelix@gmail.com>
2016-11-05 20:06:39 +0000

    adds accessing context and formatters example

---
+++ src/AccessingHttpContext/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace AccessingHttpContext.Controllers
+{
+    [Route("api/[controller]")]
+    public class ResourcesController : Controller
+    {
+        private readonly HttpContext httpContext;
+
+        //private readonly IHttpContextAccessor httpContextAccessor;
+
+        public ResourcesController(
+            // IHttpContextAccessor httpContextAccessor
+            HttpContext httpContext
+            )
+        {
+            //this.httpContextAccessor = httpContextAccessor;
+            this.httpContext = httpContext;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            //return httpContextAccessor.HttpContext.Request.Headers["user-agent"];
+            return httpContext.Request.Headers["user-agent"];
+        }
+    }
+}
---
+++ src/CustomMediaTypeResponse/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomMediaTypeResponse.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public ErrorModel Get()
+        {
+            Response.StatusCode = 403;
+            Response.Headers["content-type"] = "application/problem+json";
+            return new ErrorModel
+            {
+                Type = new Uri("https://example.com/cant/touch/this"),
+            };
+        }        
+    }
+
+    public class ErrorModel
+    {
+        public Uri Type { get; set; }
+    }
+}
